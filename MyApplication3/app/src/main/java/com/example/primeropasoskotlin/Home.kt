768c58a6f2dc49c898941ef50967b725216d5a66
KendCode <kenderacademico@gmail.com>
2024-08-21 21:24:50 -0400

    se añadio productos y se modifico login

--- MyApplication3/app/src/main/java/com/example/primeropasoskotlin/Home.kt
+++ MyApplication3/app/src/main/java/com/example/primeropasoskotlin/Home.kt
@@ -1,12 +1,17 @@
 package com.example.primeropasoskotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Home : AppCompatActivity() {
+    lateinit var carProducto:CardView
+    lateinit var carIva:CardView
+    lateinit var carSalir:CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,26 @@ class Home : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        cargarR()
+        estadoBoton()
+    }
+
+    fun cargarR (){
+        carProducto = findViewById(R.id.cardProducto)
+        carIva = findViewById(R.id.cardIva)
+        carSalir = findViewById(R.id.cardSalida)
+    }
+
+    fun estadoBoton(){
+        carIva.setOnClickListener{
+            val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+        }
+        carProducto.setOnClickListener{
+            val f = Intent(this,Productos::class.java)
+            startActivity(f)
+        }
+
+
     }
 }
--- MyApplication3/app/src/main/java/com/example/primeropasoskotlin/Productos.kt
+++ MyApplication3/app/src/main/java/com/example/primeropasoskotlin/Productos.kt
@@ -1,39 +1,20 @@
 package com.example.primeropasoskotlin
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Login : AppCompatActivity() {
-
-    lateinit var btnLogin:Button
-
-
+class Productos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_productos)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        cargarR()
-        estadoBoton()
-    }
-    //CREAR FUNCION CARGAR R
-    fun cargarR(){
-       btnLogin = findViewById(R.id.loginButton)
-    }
-
-    fun estadoBoton(){
-        btnLogin.setOnClickListener{
-            var aa = Intent(this, Home::class.java)
-            startActivity(aa)
         }
     }
 }
